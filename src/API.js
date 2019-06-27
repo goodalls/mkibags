de291eb89b380edbfd002fbd3c291a7f15769a2f
Jeff Goodall <goodalls@live.com>
2019-06-26 21:17:53 -0600

    adding fetch call with auth. untested

--- src/API.js
+++ src/API.js
@@ -1,10 +1,18 @@
+import { consumerKey } from './key';
+import { secretKey } from './key';
+
 export const fetchParse = async url => {
   try {
-    const initialFetch = await fetch(url);
+    const initialFetch = await fetch('https://mkibags.com/wc-api/v3/products/categories', {
+      method: 'get',
+      headers: new Headers({
+        'Authorization': 'Basic ' + btoa({ consumerKey } + ':' + { secretKey }),
+        'Content-Type': 'application/json'
+      })
+    });
     const response = await initialFetch.json();
     return response;
   } catch (err) {
-    return 'fetchParse error '+ err;
+    return 'fetchParse error ' + err;
   }
 };
-
