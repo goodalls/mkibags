4a4ac09900d1895e2b4dfcc536ec82b1593102ad
Jeff Goodall <goodalls@live.com>
2019-07-03 13:00:33 -0600

    adding header files and Nav component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Header.scss';
+import Nav from '../Nav/Nav';
 
 const Header = () => {
   return (
     <div className='Header'>
-      <div className='one'>1</div>
-      <div className='two'>2</div>
-      <div className='three'>3</div>
+      <div className='one'>
+      1
+      
+      </div>
+      <div className='two'>
+        <Nav/>
+      </div>
     </div>
   );
 };
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className="Nav">
+      Navigation goes here
+    </div>
+  );
+}
+
+export default Nav;
