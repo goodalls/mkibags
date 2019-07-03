667134f8b6b4d2698b229573e6cac2c1c8a5301b
Jeff Goodall <goodalls@live.com>
2019-07-03 13:24:25 -0600

    adding css and links to Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './Nav.scss';
 
 function Nav() {
   return (
     <div className="Nav">
       Navigation goes here
+      <a href="">link</a>
+      <a href="">link</a>
+      <a href="">link</a>
+      <a href="">link</a>
     </div>
   );
 }
